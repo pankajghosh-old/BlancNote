72dbabd46fa2475178c85b0f0097a495a676a460
Pankaj Ghosh <pankaj.ghosh@gmail.com>
2014-04-24 20:51:45 -0400

    added robots.txt

--- sitebuilder.py
+++ sitebuilder.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask.ext.flatpages import FlatPages
+from flask.helpers import send_from_directory
+from flask.globals import request
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -17,6 +19,11 @@ def index():
 def tag(tag):
     tagged = [p for p in pages if tag in p.meta.get('tags', [])]
     return render_template('tag.html', pages=tagged, tag=tag)
+
+@app.route('/robots.txt')
+# @app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 @app.route('/<path:path>/')
 def page(path):
